ea974c247f021705306dc0895b70864c3c2b37a7
kaiyuan01 <kaiyuan01@users.noreply.github.com>
2024-03-22 20:45:32 +0000

    u

--- src/main/java/com/dengsoft/tutorial/springboot/emp/util/EmpUtils.java
+++ src/main/java/com/dengsoft/tutorial/springboot/emp/util/EmpUtils.java
@@ -1,5 +1,8 @@
 package com.dengsoft.tutorial.springboot.emp;
 
+/* java -cp . src/main/java/com/dengsoft/tutorial/springboot/emp/util/EmpUtils.java */
 public class EmpUtils {
-
+    public static void main(String[] args) {
+		System.out.println("EmpUtils was called");
+	}
 }
